cb5f9f75bd2599b50f7b0d7aeaf74eb5b65adc8e
makao007 <makao007@126.com>
2026-06-04 10:29:41 +0800

    x

--- nava_src/to_sdnq.py
+++ nava_src/to_sdnq.py
@@ -5,6 +5,7 @@ from sdnq import SDNQConfig
 from sdnq.loader import save_sdnq_model, apply_sdnq_options_to_model
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.nava.modules.t5 import umt5_xxl, T5EncoderModel
 
 def get_sdnq_config ():
